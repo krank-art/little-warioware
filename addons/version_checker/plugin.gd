a289edf82b933a51b9f4d67925f0ad3b39132ec8
Krank <kranko.art@gmail.com>
2024-05-08 02:16:35 +0200

    Update version checker

--- addons/version_checker/plugin.gd
+++ addons/version_checker/plugin.gd
@@ -1,8 +1,8 @@
-tool
+@tool
 extends EditorPlugin
 
-const EXPECTED_MAJOR: int = 3
-const EXPECTED_MINOR: int = 5
+const EXPECTED_MAJOR: int = 4
+const EXPECTED_MINOR: int = 2
 const EXPECTED_PATCH: int = -1  # -1 here means we don't care about the patch number
 
 
@@ -20,13 +20,13 @@ func _enter_tree() -> void:
 
 func fail(version: String) -> void:
 	var dialog: AcceptDialog = AcceptDialog.new()
-	dialog.window_title = "Invalid version"
+	dialog.title = "Invalid version"
 	dialog.dialog_text = (
 		"This project is not compatible with the version of Godot you are using (%s)"
 		% version
 	)
-	dialog.connect("confirmed", dialog, "hide")
-	dialog.connect("popup_hide", dialog, "queue_free")
+	dialog.connect("confirmed", dialog.hide)
+	dialog.connect("canceled", dialog.queue_free)
 
 	get_editor_interface().get_base_control().add_child(dialog)
 
